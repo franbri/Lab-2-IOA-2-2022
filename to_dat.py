83a0c5b2a098d6057ed10cebda144fb7aee7a7a4
Francisco Briceño <francisco.briceno.a@mail.pucv.cl>
2022-11-28 01:53:38 -0300

    commented to_dat.py

--- to_dat.py
+++ to_dat.py
@@ -1,16 +1,17 @@
 from OR import read_file
 import os, sys
 
+# this function converts all OR-library cap* problems in the datasets folder, and creates a corresponding dat file in the "processed_datasets" folder
 def to_dat(filename):
     cli, loc, FC, ICap, dem, TC = read_file('datasets/' + filename)
     name = filename.split('.')[0]
     with open('processed_datasets/' + name + '.dat', 'w') as dat:
 
-        #clientes y centros
+        #clients and centers
         dat.write("param cli := " + str(cli) +';\n')
         dat.write("param loc := " + str(loc) +';\n')
 
-        #costos de instalacion
+        #Facility costs
         dat.write("param FC := ")
         for idC, C in enumerate(FC):
             if(idC+1 != len(FC)):
@@ -19,7 +20,7 @@ def to_dat(filename):
                 dat.write(str(idC+1) + '\t' + str(C))
         dat.write(';\n\n')
 
-        #Capacidad de cada centro
+        #Facility capacity
         dat.write("param ICap := ")
         for idC, C in enumerate(ICap):
             if(idC+1 != len(ICap)):
@@ -28,6 +29,7 @@ def to_dat(filename):
                 dat.write(str(idC+1) + ' ' + str(C))
         dat.write(';\n\n')
 
+        #Customer demand
         dat.write("param dem := ")
         for idC, C in enumerate(dem):
             if(idC+1 != len(dem)):
@@ -35,7 +37,7 @@ def to_dat(filename):
             else:
                 dat.write(str(idC+1) + ' ' + str(C))
         dat.write(';\n\n')
-
+        #Transport Cost
         dat.write("param TC : ")
         for i in range(1, len(TC[0]) + 1):
             if(i != len(TC[0])):
@@ -52,10 +54,13 @@ def to_dat(filename):
 
         dat.close()
 
+# guard to not execute when importing
 if __name__ == "__main__":
+    # if a name is given, only convert the especified file
     if (len(sys.argv) < 2):
         for filename in os.listdir("datasets"):
             to_dat(filename)
+    # else convert all files in datasets folder
     else:
         to_dat(sys.argv[1])
 
