7001b7cb7c60234a58c55dda4c6c94b04f7ddfcf
Francisco Briceño <francisco.briceno.a@mail.pucv.cl>
2022-11-28 01:53:28 -0300

    Commented OR.py

--- OR.py
+++ OR.py
@@ -1,12 +1,12 @@
 import sys
 from math import ceil
 
+# this program purpose is to read OR library problems from txt python variables
 def read_file(filename):
     file = open(filename, "r")
     nloc, ncus = file.readline().strip().split(" ")
     loc = int(nloc)
     cli = int(ncus)
-    #print(loc, cli)
 
     ICap = []
     FC = []
@@ -14,8 +14,6 @@ def read_file(filename):
         temp = file.readline().strip().split(" ")
         ICap.append(int(temp[0]))
         FC.append(float(temp[1]))
-    #print(ICap)
-    #print(FC)
 
     iter_temp = ceil(loc/7)
     dem = []
@@ -27,10 +25,9 @@ def read_file(filename):
         for indexCustomerLoc in range(iter_temp):
             temp_TC = temp_TC + list(map(float, file.readline().strip().split(" ")))
         TC.append(temp_TC)
-    #print(dem)
-    #print(type(TC))
 
     return cli, loc, FC, ICap, dem, TC
 
+# guard to not execute this when importing
 if __name__ == "__main__":
     read_file(sys.argv[1])
